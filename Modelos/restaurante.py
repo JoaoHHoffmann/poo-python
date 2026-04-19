16bd4394230477ec5f576011f959c5970d6ca14a
JoaoHoffmann <joaohhoffmann16@gmail.com>
2026-04-19 17:03:53 -0300

    Ajustando o construtor

--- Modelos/restaurante.py
+++ Modelos/restaurante.py
@@ -1,13 +1,13 @@
 class Restaurante:
     def __init__(self, nome, categoria):
-        self.nome = ''
-        self.categoria = ''
-        ativo  = False
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
 
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
+restaurantes = [restaurante_praca, restaurante_pizza]
 
-restaurante_praca = Restaurante()
-restaurante_pizza = Restaurante()
-
-
-
+print(vars(restaurante_praca))
+print(vars(restaurante_pizza))
