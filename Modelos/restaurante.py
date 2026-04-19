2e09a88c502b519261be1d8cfa36c7222f954236
JoaoHoffmann <joaohhoffmann16@gmail.com>
2026-04-19 16:54:02 -0300

    Criando o construtor

--- Modelos/restaurante.py
+++ Modelos/restaurante.py
@@ -1,10 +1,13 @@
 class Restaurante:
-    def __init__(self, nome, categoria, ativo):
-        pass
+    def __init__(self, nome, categoria):
+        self.nome = ''
+        self.categoria = ''
+        ativo  = False
+
+
 
 restaurante_praca = Restaurante()
 restaurante_pizza = Restaurante()
 
-restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurantes)
+
