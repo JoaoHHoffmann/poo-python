bb3dd69362c6487ea68e465ba2b5129851d2cad1
JoaoHoffmann <joaohhoffmann16@gmail.com>
2026-04-19 16:40:48 -0300

    Criando a classe restaurante

---
+++ Modelos/restaurante.py
@@ -0,0 +1,10 @@
+class Restaurante:
+    def __init__(self, nome, categoria, ativo):
+        pass
+
+restaurante_praca = Restaurante()
+restaurante_pizza = Restaurante()
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+
+print(restaurantes)
