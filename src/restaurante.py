f78c02fb26bebe9017df86c69febff9de150c8a4
JoaoHoffmann <joaohhoffmann16@gmail.com>
2026-04-19 17:24:56 -0300

    Adicionando os restuartntes em uma lista e criando o método para listar eles

--- src/restaurante.py
+++ src/restaurante.py
@@ -1,16 +1,21 @@
 class Restaurante:
+    restaurantes = []
+
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
 
     def __str__(self):
         return f'{self.nome} - {self.categoria} - {"Ativo" if self.ativo else "Inativo"}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} - {restaurante.categoria} - {"Ativo" if restaurante.ativo else "Inativo"}')
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurante_praca)
-print(restaurante_pizza)
+Restaurante.listar_restaurantes()
