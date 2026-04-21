d0cf8941b8a29fc21957d2e69e153e84ae1f45e9
JoaoHoffmann <joaohhoffmann16@gmail.com>
2026-04-20 22:18:53 -0300

    Ajustando para quando o restaurante  não tiver avaliacoes

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from src.restaurante import Restaurante
 
 restaurante_praca = Restaurante('Restaurante da Praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('João', 5)
-restaurante_praca.receber_avaliacao('Maria', 8)
-restaurante_praca.receber_avaliacao('Pedro', 7)
+# restaurante_praca.receber_avaliacao('João', 5)
+# restaurante_praca.receber_avaliacao('Maria', 8)
+# restaurante_praca.receber_avaliacao('Pedro', 7)
 
 def main():
     Restaurante.listar_restaurantes()
