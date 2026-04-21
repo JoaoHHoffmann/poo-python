0951565f0d0fe0edf09fd4819f2443c1cebf1e30
JoaoHoffmann <joaohhoffmann16@gmail.com>
2026-04-20 21:56:37 -0300

    Ajustando

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from src.restaurante import Restaurante
 
-restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_mexicano = Restaurante('Mexican food', 'Mexicana')
-restaurante_japones = Restaurante('Japa', 'Japonesa')
-
-restaurante_mexicano.alternar_estado()
+restaurante_praca = Restaurante('Restaurante da Praça', 'Gourmet')
+restaurante_praca.receber_avaliacao('João', 5)
+restaurante_praca.receber_avaliacao('Maria', 8)
+restaurante_praca.receber_avaliacao('Pedro', 7)
 
 def main():
     Restaurante.listar_restaurantes()
 
-    
+
 if __name__ == '__main__':
     main()
 
